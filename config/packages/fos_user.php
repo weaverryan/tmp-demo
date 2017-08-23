42df659c37aba34b8682938420687a9853e87d42
Ryan Weaver <ryan@thatsquality.com>
2017-08-23 13:21:22 -0400

    updating fos_user

--- config/packages/fos_user.php
+++ config/packages/fos_user.php
@@ -1,23 +1,27 @@
 <?php
 
-use Symfony\Component\Config\Loader\Config\fos_user;
+use App\AppConfigurator;
 
-fos_user\db_driver('orm');
-fos_user\firewall_name('main');
-fos_user\user_class('App\\Entity\\User');
-fos_user\service()
-  ->user_manager(UserManager::class)
-  ->mailer(CustomMailer::class);
-fos_user\from_email()
-  ->address('%mailer_sender_address%')
-  ->sender_name('%mailer_sender_name%');
+return function(AppConfigurator $di) {
+    $fos_user = $di->fos_user();
 
-// we go one level deeper here
-fos_user\registration\confirmation()
-  ->enabled(true);
-fos_user\registration\form()
-  ->type(RegistrationFormType::class);
+    $fos_user->db_driver('orm');
+    $fos_user->firewall_name('main');
+    $fos_user->user_class('App\\Entity\\User');
+    $fos_user->service()
+      ->user_manager(UserManager::class)
+      ->mailer(CustomMailer::class);
+    $fos_user->from_email()
+      ->address('%mailer_sender_address%')
+      ->sender_name('%mailer_sender_name%');
 
-fos_user\resetting\token_ttl(86400);
-fos_user\change_password(true);
-fos_user\profile(true);
+    // we go one level deeper here
+    $fos_user->registration\confirmation()
+      ->enabled(true);
+    $fos_user->registration\form()
+      ->type(RegistrationFormType::class);
+
+    $fos_user->resetting\token_ttl(86400);
+    $fos_user->change_password(true);
+    $fos_user->profile(true);
+};
